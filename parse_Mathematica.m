dbca1a16aefd8295a41f35be59e440b2a7b44812
Justin <justinthomas@jtwebs.net>
2012-09-10 14:28:41 -0400

    Updates parse_Mathematica

--- parse_Mathematica.m
+++ parse_Mathematica.m
@@ -3,15 +3,30 @@ function str = parse_Mathematica(str)
 str = strrep(str, '[t]', '');
 str = strrep(str, 'Sin', 'sin');
 str = strrep(str, 'Cos', 'cos');
-str = strrep(str, '\[', '[');
+% str = strrep(str, '\[', '[');
 % str = strrep(str, '^[power]', 'dot');
-str = strrep(str, ' ', '*');
 % regexprep
 
-str = strrep(str, '[Beta]^[Prime]', 'b1');
-str = strrep(str, '[Beta]', 'b0');
-str = strrep(str, '[Theta]', 'th0');
+str = strrep(str, '\[Beta]^\[Prime]\[Prime]', 'b2');
+str = strrep(str, '\[Beta]^\[Prime]', 'b1');
+str = strrep(str, '\[Beta]^(3)', 'b3');
+str = strrep(str, '\[Beta]^(4)', 'b4');
+str = strrep(str, '\[Theta]^\[Prime]\[Prime]', 'th2');
+str = strrep(str, '\[Theta]^\[Prime]', 'th1');
+str = strrep(str, '\[Theta]', 'th0');
+str = strrep(str, 'xq^\[Prime]\[Prime]', 'x2');
+str = strrep(str, 'zq^\[Prime]\[Prime]', 'z2');
+str = strrep(str, 'xq^(3)', 'x3');
+str = strrep(str, 'zq^(3)', 'z3');
+str = strrep(str, 'xq^(4)', 'x4');
+str = strrep(str, 'zq^(4)', 'z4');
+str = strrep(str, '\[Sqrt]','sqrt');
 
+% Anything else
+str = strrep(str, '\[Beta]', 'b0');
+str = strrep(str, '/', './');
+str = strrep(str, ' ', '.*');
+str = strrep(str, '^', '.^');
 str = strrep(str, '[', '(');
 str = strrep(str, ']', ')');
 
