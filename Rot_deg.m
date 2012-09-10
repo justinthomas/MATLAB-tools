db8413b1a0c495e8b0851e1794c461181b317c21
Justin <justinthomas@jtwebs.net>
2012-09-09 23:44:45 -0400

    Because sin(pi) is not equal to 0

---
+++ Rot_deg.m
@@ -0,0 +1,29 @@
+function R = Rot_deg(theta, ax)
+% Creates a rotation matrix that is rotated an angle of theta about the
+% specified axis.  Note: use 'x', 'y', 'z' for the ax argument.
+
+
+switch ax
+    case {'x', 'X', 1}
+        
+        R = [...
+            1   0       0
+            0   cosd(theta) -sind(theta)
+            0   sind(theta) cosd(theta)];
+        
+    case {'y', 'Y', 2}
+        
+        R = [...
+            cosd(theta)  0   sind(theta)
+            0           1   0
+            -sind(theta) 0   cosd(theta)];
+        
+    case {'z', 'Z', 3}
+        
+        R = [...
+            cosd(theta)  -sind(theta) 0
+            sind(theta)  cosd(theta)  0
+            0           0           1];
+end
+
+end
