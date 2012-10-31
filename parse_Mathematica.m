6fe3f849018a86db44926c45cd436b4b5fb14bcd
Justin <justinthomas@jtwebs.net>
2012-10-31 17:16:19 -0400

    Updates parse_Mathematica to handle exported expressions from Mathematica

--- parse_Mathematica.m
+++ parse_Mathematica.m
@@ -13,6 +13,7 @@ str = strrep(str, '\[Beta]^\[Prime]\[Prime]', 'b2');
 str = strrep(str, '\[Beta]^\[Prime]', 'b1');
 str = strrep(str, '\[Beta]^(3)', 'b3');
 str = strrep(str, '\[Beta]^(4)', 'b4');
+str = strrep(str, 'θ', '\[Theta]');
 str = strrep(str, '\[Theta]^\[Prime]\[Prime]', 'th2');
 str = strrep(str, '\[Theta]^\[Prime]', 'th1');
 str = strrep(str, '\[Theta]', 'th0');
@@ -30,9 +31,10 @@ str = strrep(str, 'Sqrt', 'sqrt');
 % Anything else
 str = strrep(str, '\[Beta]', 'b0');
 str = strrep(str, '/', './');
-str = strrep(str, ' ', '.*');
+str = strrep(str, '*', '.*');
 str = strrep(str, '^', '.^');
 str = strrep(str, '[', '(');
 str = strrep(str, ']', ')');
+str = strrep(str, '==', '=');
 
 end
