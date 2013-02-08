89e8c5c97dff89b1aac665e03f8832eb409821cf
Justin Thomas <jut@seas.upenn.edu>
2013-02-08 15:15:39 -0500

    TrimVecLength is used to trim the beginning from a vector to maintain a certain length.  Future improvements will allow trimming from the end.

---
+++ TrimVecLength.m
@@ -0,0 +1,14 @@
+function vec = TrimVecLength(vec, len, trim_beginning)
+%function vec = TrimVecLength(vec, length, trim_from)
+%TrimVecLength trims a vector to length len by removing from the beginning
+%if trim_beginning = 1 and removing from the end if trim_beginning = 0;
+
+if trim_beginning
+    
+    vec = vec(max(length(vec) - len + 1,1):end);
+    
+else
+    
+    % #### Should make this work...
+    
+end
