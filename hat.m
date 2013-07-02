8a3cc563db9a51c6c2cccb7ab863b9ad66e163ea
Justin Thomas <jut@seas.upenn.edu>
2013-07-02 18:20:35 -0400

    Adds 2x2 capability to the hat operator

--- hat.m
+++ hat.m
@@ -1,11 +1,20 @@
 function ss = hat(vec)
 % function ss = hat(vec)
-%   The hat function maps a vector in R^3 to its
-%   skew symmetric matrix in R^(3x3)
+%   The hat function maps a vector in R^3 (or R^1) to its
+%   skew symmetric matrix in R^(3x3) (or R^(2x2))
 
-ss = [...
-    0, -vec(3), vec(2);...
-    vec(3), 0, -vec(1);...
-    -vec(2), vec(1), 0];
+switch length(vec)
+    
+    case 3
+        ss = [...
+            0, -vec(3), vec(2);...
+            vec(3), 0, -vec(1);...
+            -vec(2), vec(1), 0];
+        
+    case 1
+        ss = [...
+            0, -vec; ...
+            vec, 0];
+end
 
 end
